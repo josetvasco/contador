42407b0b10c6af0f40ccb7c5e281e8bb98083aec
Jose Daniel Toro <torovascojosedaniel@gmail.com>
2022-12-14 11:29:25 -0500

    agregamdo estilos a footer y titulo

---
+++ src/componens/titulo.js
@@ -0,0 +1,13 @@
+
+
+export const titulo = () => {
+
+    const titulo = document.createElement('h1');
+    titulo.innerHTML = 'WE`RE LAUNCHING SOON';
+
+    const sectionTitulo = document.createElement('section');
+    sectionTitulo.classList.add('section-titulo');
+    sectionTitulo.appendChild(titulo)
+
+    return sectionTitulo;
+}
