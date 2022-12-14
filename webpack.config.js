eaae1da32fe776b362785293961778830ef985b8
Jose Daniel Toro <torovascojosedaniel@gmail.com>
2022-12-13 19:17:25 -0500

    added config

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+
+    entry: {
+        dev: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './build')
+    },
+    devServer: {
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "myWepackServer"
+        })
+    ]
+}
